572611bce9188867b4bc63267d3b7cdc10b4d70b
Maksim Oloviannikov <olovjanqnet@gmail.com>
2024-01-28 18:14:54 +0300

    work added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import FetchBox from './components/FetchBox';
 import './App.css';
 
+const url = ' http://localhost:7070/'
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<FetchBox url={url} opts='data' />
+			<FetchBox url={url} opts='loading' />
+			<FetchBox url={url} opts='error' />
+		</div>
+	);
 }
 
 export default App;
